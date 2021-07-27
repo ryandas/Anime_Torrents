7b8549587e5ac245941336990b9de764deb9a413
ryandas <ryandas@protonmail.com>
2021-07-27 18:33:22 +0530

    bad fix to 503 issue

--- anime_torrents.py
+++ anime_torrents.py
@@ -1,21 +1,23 @@
 import requests, random, csv, os, sys, re, operator,time
+from natsort import natsorted
 from bs4 import BeautifulSoup
 
 base_address="https://animetosho.org/"
 watched = -1
+headerfile = os.path.abspath("./Chrome.txt")
 
 def random_ua(fname):
     lines = open(fname).read().splitlines()
     return random.choice(lines)
 
 def source(url):
-    headers = {"User-Agent":random_ua("/home/ryan/repos/dotfiles/scripts/anime_torrents/Chrome.txt")}
+    headers = {"User-Agent":random_ua(headerfile)}
     r=requests.get(url,headers=headers,timeout=2)
     if r.status_code == 200:
         return r.text
     if r.status_code == 503:
         print(r.status_code)
-        source(url)
+        return source(url)
     else:
         print(r.status_code) 
 
@@ -23,20 +25,20 @@ def source(url):
 def sort_csv(filename,rev):
     csvfile=open(filename,'r')
     reader = csv.reader(csvfile)
-    sortedlist = sorted(reader, key=operator.itemgetter(1), reverse=rev)
-    with open(filename, 'a') as f:
+    sortedlist = natsorted(reader)
+    with open(filename, 'w') as f:
         fileWriter = csv.writer(f, delimiter=',')
         for row in sortedlist:
             fileWriter.writerow(row)
     csvfile.close()
 
 def torrent_links(page):
-    #print("\t"+page)
+    #print("\t"+page) # debug
     soup=BeautifulSoup(source(page), "html.parser")
     anchors = soup.findAll("a")
     title = soup.findAll("h2",{"id":"title"})
     episode_number = soup.findAll("td")[2].a.next_sibling.next_sibling.text.split()[1]
-    #print(episode_number)
+    #print(episode_number) # debug
     for anchor in anchors:
         if(anchor.text=="Torrent Download"):
             try:
@@ -55,8 +57,8 @@ def link_finder(page):
     print(page)
     soup = BeautifulSoup(source(page), "html.parser")
     links = soup.findAll("div",{"class":"link"})
-    #pattern1 = re.compile("\[Erai-raws\]")
-    pattern1 = re.compile("\[HorribleSubs\]")
+    pattern1 = re.compile("\[Erai-raws\]")
+    #pattern1 = re.compile("\[HorribleSubs\]")
     pattern2 = re.compile("\[1080p\]")
     pattern3 = re.compile("~")
     pattern4 = re.compile("\[Unofficial Batch\]")
@@ -64,7 +66,7 @@ def link_finder(page):
         if(re.search(pattern1, link.text)):
             if(re.search(pattern2,link.text)):
                 if(re.search(pattern3, link.text)):
-                    #print(link.a["href"],file=lfile)
+                    #print(link.a["href"],file=lfile) # debug
                     continue
                 elif(re.search(pattern4,link.text)):
                     continue
@@ -73,7 +75,7 @@ def link_finder(page):
 
 def pages_finder(anime_details,page_number):
     url="{address}?page={i}".format(address=anime_details["address"],i=page_number)
-    #print(url)
+    #print(url) # debug
     soup = BeautifulSoup(source(url), "html.parser")
     date = soup.find("div",{"class":"home_list_datesep"})
     if date.text != "-":
@@ -83,57 +85,57 @@ def pages_finder(anime_details,page_number):
     else:
         return
 
-#if __name__ == "__main__":
-search_query='+'.join(sys.argv[1:])
-url = "{address}/search?q={query}".format(address=base_address,query=search_query)
-print(url)
+if __name__ == "__main__":
+    search_query='+'.join(sys.argv[1:])
+    url = "{address}/search?q={query}".format(address=base_address,query=search_query)
+    print(url)
 
-soup = BeautifulSoup(source(url), "html.parser")
+    soup = BeautifulSoup(source(url), "html.parser")
 
-series_set = set(soup.findAll("span",{"class":"serieslink"}))
-series_dict = dict()
-for num,series in enumerate(series_set):
-    print(str(num) + " " + series.text + " : " + series.a["href"])
-    series_dict.setdefault(num,series)
+    series_set = set(soup.findAll("span",{"class":"serieslink"}))
+    series_dict = dict()
+    for num,series in enumerate(series_set):
+        print(str(num) + " " + series.text + " : " + series.a["href"])
+        series_dict.setdefault(num,series)
 
-i = int(input("Which anime : "))
+    i = int(input("Which anime : "))
 
-anime_details={
-    "address": series_dict.get(i).a["href"],
-    "title": series_dict.get(i).text,
-}
+    anime_details={
+        "address": series_dict.get(i).a["href"],
+        "title": series_dict.get(i).text,
+    }
 
-try:
-    os.mkdir(anime_details["title"])
-except OSError:
-    print(anime_details["title"] + " directory aleady exists")
-    os.chdir(anime_details["title"])
-    csvfile = open("main.csv",'r')
-    reader = csv.reader(csvfile)
     try:
-        watched = next(reader)[0]
-    except:
-        watched = -1
-    csvfile.close()
-    os.chdir("../")
+        os.mkdir(anime_details["title"])
+    except OSError:
+        print(anime_details["title"] + " directory aleady exists")
+        os.chdir(anime_details["title"])
+        csvfile = open("main.csv",'r')
+        reader = csv.reader(csvfile)
+        try:
+            watched = next(reader)[0]
+        except:
+            watched = -1
+        csvfile.close()
+        os.chdir("../")
 
-os.chdir(anime_details["title"])
+    os.chdir(anime_details["title"])
 
-fieldnames = ["episode_number","title","torrent"]
+    fieldnames = ["episode_number","title","torrent"]
 
-download = open("to_download.csv",'w')
-main_csv = open("main.csv",'a')
-special = open("special.csv",'a')
+    download = open("to_download.csv",'w')
+    main_csv = open("main.csv",'a')
+    special = open("special.csv",'a')
 
-main_write = csv.DictWriter(main_csv,fieldnames=fieldnames)
-d_write = csv.DictWriter(download,fieldnames=fieldnames)
-special_write = csv.DictWriter(special,fieldnames=fieldnames)
+    main_write = csv.DictWriter(main_csv,fieldnames=fieldnames)
+    d_write = csv.DictWriter(download,fieldnames=fieldnames)
+    special_write = csv.DictWriter(special,fieldnames=fieldnames)
 
-pages_finder(anime_details,1)
+    pages_finder(anime_details,1)
 
-main_csv.close()
-download.close()
-special.close()
+    main_csv.close()
+    download.close()
+    special.close()
 
-sort_csv("main.csv",True)
-#sort_csv("to_download.csv",False
+    sort_csv("main.csv",True)
+    #sort_csv("to_download.csv",False
